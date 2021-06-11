f600c29245e51fd3dae09410c4a4a054d590eb3e
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-11 21:20:47 +0900

    Add file for if and swich statements

---
+++ decision-structures/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "log"
+
+func main() {
+	cat := "cat"
+
+	if cat == "cat" {
+		log.Println("Cat is cat")
+	} else {
+		log.Println("Cat is not cat")
+	}
+
+	myNum := 100
+	isTrue := false
+
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is greater than 99 and isTrue is set to true")
+	} else if myNum < 100 && !isTrue {
+		log.Println("1")
+	} else if myNum == 101 || isTrue {
+		log.Println("2")
+	} else if myNum > 1000 && isTrue == false {
+		log.Println("3")
+	}
+
+	myVar := "cat"
+
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+	case "dog":
+		log.Println("cat is set to dog")
+	case "fish":
+		log.Println("cat is set to fish")
+	default:
+		log.Println("cat is something else")
+	}
+}
